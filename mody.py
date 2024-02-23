68ac9627fe8048e2374b1b1c9e2dbc09ab21a254
alivredt <161015670+alivredt@users.noreply.github.com>
2024-02-23 16:11:06 +0300

    Update mody.py

--- mody.py
+++ mody.py
@@ -4,5 +4,5 @@ import os
 APP_ID = os.environ.get("APP_ID", "29035333")
 APP_HASH = os.environ.get("APP_HASH", "85d2357d1e0e5b3399aaa3c7bf01dd4f")
 SESSION = os.environ.get("SESSION")
-elhzeyba = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
+elhzeyba = TelegramClient(StringSession
 elhzeyba.start()
